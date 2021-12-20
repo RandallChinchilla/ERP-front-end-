cc3c71288ee8166a6580f57f9f5da97f86850b29
scanobaq <38079857+scanobaq@users.noreply.github.com>
2021-12-20 11:33:28 -0500

    se agrega select para tipo de comprobante

--- ceo.web/src/Componets/Listas/SelectTipoComprobante.js
+++ ceo.web/src/Componets/Listas/SelectTipoComprobante.js
@@ -1,41 +1,44 @@
-import {
-    Select,
-    MenuItem,
-  } from "@mui/material";
+import { Select, MenuItem } from "@mui/material";
 import React from "react";
 import InputLabel from "@mui/material/InputLabel";
 import { useGetData } from "../../Hooks/useGetData";
 
-export default function SelectId({form, handleBlur, handleChange}){
+export default function SelectTipoComprobante({
+  form,
+  handleBlur,
+  handleChange,
+}) {
+  const { Data, Error, setData } = useGetData(
+    "FelTipoDocumento/GetFelTipoDocumentos"
+  );
 
-      const { Data, Error, setData } = useGetData("FelTipoDocumento");
-    
-      if (Error) return null;
-      if (!Data) return null;
+  if (Error) return null;
+  if (!Data) return null;
 
-      let options = Data;
+  let options = Data;
 
-      return(
-          <>
-           <InputLabel id="demo-simple-select-label">Tipo Comprobante</InputLabel>
-           <Select
-            labelId="demo-simple-select-label"
-            id="codigoTipoDocumento"
-            name="codigoTipoDocumento"
-            label="codigoTipoDocumento"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={form.codigoTipoDocumento}
-          >
-            {Data &&
-             options.map((item) => (
-             <MenuItem
-             key={item.codigoTipoDocumento}
-             value={item.codigoTipoDocumento}
-             >
-               {item.descripcion}
-               </MenuItem>
-             ))}
-          </Select>
-          </>
-      )}
+  return (
+    <>
+      <InputLabel id="demo-simple-select-label">Tipo Comprobante</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="CodigoTipoDocumento"
+        name="CodigoTipoDocumento"
+        label="CodigoTipoDocumento"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={form.CodigoTipoDocumento}
+      >
+        {Data &&
+          options.map((item) => (
+            <MenuItem
+              key={item.codigoTipoDocumento}
+              value={item.codigoTipoDocumento}
+            >
+              {item.descripcion}
+            </MenuItem>
+          ))}
+      </Select>
+    </>
+  );
+}
